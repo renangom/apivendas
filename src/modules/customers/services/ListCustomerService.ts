34e61ed0d0f767483d2c51187cdb84fb3cbb2f1a
Renan <renan23eduardo@gmail.com>
2022-09-12 10:07:30 -0300

    Fix ListCustomer and UpdateCustomer

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,12 +1,13 @@
+import { getCustomRepository } from "typeorm";
 import Customer from "../typeorm/entities/Customer";
-import { CustomerRepository } from "../typeorm/repositories/CustomerRepository";
+import CustomersRepository from "../typeorm/repositories/CustomerRepository";
 
 
 
 
 class ListCustomerService{
   public async execute():Promise<Customer[]>{
-    const customerRepository = new CustomerRepository();
+    const customerRepository = getCustomRepository(CustomersRepository)
 
     const customers = await customerRepository.find();
 
--- src/modules/customers/services/UpdateCustomerService.ts
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -1,6 +1,7 @@
 import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
 import Customer from "../typeorm/entities/Customer";
-import { CustomerRepository } from "../typeorm/repositories/CustomerRepository";
+import CustomersRepository from "../typeorm/repositories/CustomerRepository";
 
 
 interface IRequest{
@@ -12,7 +13,7 @@ interface IRequest{
 
 class UpdateCustomerService{
   public async execute({id, name, email}:IRequest):Promise<Customer>{
-    const customerRepository = new CustomerRepository();
+    const customerRepository = getCustomRepository(CustomersRepository);
 
     const customer = await customerRepository.findById(id);
 
