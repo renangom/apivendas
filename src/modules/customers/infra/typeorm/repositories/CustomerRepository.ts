dec09f88f8cfc482fca85d147961055378f4fe3f
Renan <renan23eduardo@gmail.com>
2022-09-26 14:51:02 -0300

    Refact TypeORM Customer Repository

--- src/modules/customers/infra/typeorm/repositories/CustomerRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomerRepository.ts
@@ -1,11 +1,33 @@
+import { ICreateCustomer } from '@modules/customers/domain/models/ICreateCustomer';
 import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomerRepository';
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 import Customer from '../entities/Customer';
 
-@EntityRepository(Customer)
-class CustomersRepository extends Repository<Customer> implements ICustomersRepository {
+
+
+class CustomersRepository implements ICustomersRepository {
+  private ormRepository: Repository<Customer>;
+
+  constructor(){
+    this.ormRepository = getRepository(Customer)
+  }
+
+  public async create({name,email}:ICreateCustomer):Promise<Customer>{
+    const customer = this.ormRepository.create({name, email});
+
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
+  public async save(customer:Customer):Promise<Customer>{
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
   public async findByName(name: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         name,
       },
@@ -15,7 +37,7 @@ class CustomersRepository extends Repository<Customer> implements ICustomersRepo
   }
 
   public async findById(id: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         id,
       },
@@ -25,7 +47,7 @@ class CustomersRepository extends Repository<Customer> implements ICustomersRepo
   }
 
   public async findByEmail(email: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         email,
       },
